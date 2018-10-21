a10a280680aaccf0827be9e58e3c2dcfa8edf6ef
Röttger <mona.roettger@gmail.com>
2018-10-22 01:46:34 +0200

    add a simple Login-Design and create an new class

--- app/src/main/java/com/example/mona/digitalrecipe/LoginActivity.java
+++ app/src/main/java/com/example/mona/digitalrecipe/LoginActivity.java
@@ -2,12 +2,24 @@ package com.example.mona.digitalrecipe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = (EditText)findViewById(R.id.etUsername);
+        etPassword = (EditText)findViewById(R.id.etPassword);
+    }
+
+    public void onLogin(View view){
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
     }
 }
---
+++ app/src/main/java/com/example/mona/digitalrecipe/Recipe.java
@@ -0,0 +1,4 @@
+package com.example.mona.digitalrecipe;
+
+public class Recipe {
+}
