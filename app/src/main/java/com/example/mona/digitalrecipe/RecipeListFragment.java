3b49e0446912c82ae5fed9e2886b466115ea3c4d
Röttger <mona.roettger@gmail.com>
2018-11-28 02:56:37 +0100

    function from the tab bar works

--- app/src/main/java/com/example/mona/digitalrecipe/RecipeListFragment.java
+++ app/src/main/java/com/example/mona/digitalrecipe/RecipeListFragment.java
@@ -3,7 +3,9 @@ package com.example.mona.digitalrecipe;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +13,27 @@ import android.view.ViewGroup;
 public class RecipeListFragment extends Fragment {
 
     View view;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
+
         view = inflater.inflate(R.layout.fragment_recipe_lists, container, false);
+
+        tabLayout = (TabLayout) view.findViewById(R.id.tablayout_id);
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager_id);
+        viewAdapter = new ViewPagerAdapter(getFragmentManager());
+        //adding Fragments
+        viewAdapter.addFragment(new RequiresFragment(), "Anforderungen");
+        viewAdapter.addFragment(new RecipesFragment(), "Rezepte");
+        //adapter setup
+        viewPager.setAdapter(viewAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
         return view;
     }
 }
