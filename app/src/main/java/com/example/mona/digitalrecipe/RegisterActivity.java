0cf7302eccf8d5c3ae452a74eb7f716a4aa54c94
Röttger <mona.roettger@gmail.com>
2018-11-28 18:46:07 +0100

    first design for the registration

---
+++ app/src/main/java/com/example/mona/digitalrecipe/RegisterActivity.java
@@ -0,0 +1,47 @@
+package com.example.mona.digitalrecipe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+    //Variables
+    private EditText etUsername, etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //initialise
+        etUsername = (EditText) findViewById(R.id.et_Username);
+        etPassword = (EditText) findViewById(R.id.et_Password);
+    }
+
+    //execute from Login-Button
+    public void onLogin(View view){
+        //get text from the userinputs
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        String type = "login";
+        String userrole = "Patienten";
+
+        //create instance of BackgroundWorker Class
+        BackgroundHandler backgroundHandler = new BackgroundHandler(this);
+        backgroundHandler.execute(type, userrole, username, password);
+    }
+
+    //execute from Register-Button
+    public void onRegister(View view){
+        //get text from the userinputs
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        String type = "register";
+        String userrole = "Patienten";
+
+        //create instance of BackgroundWorker Class
+        BackgroundHandler backgroundHandler = new BackgroundHandler(this);
+        backgroundHandler.execute(type, userrole, username, password);
+    }
+}
